16ed08a1442603e4d9e646073949d1f818d68d3f
小米轰炸姬 <xiaomihongzhaji@gmail.com>
2025-06-30 14:18:44 +0800

    尝试切换至pgsql

--- src/main/java/com/lwf/ytlivechatanalyse/dao/LiveChatDataMapper.java
+++ src/main/java/com/lwf/ytlivechatanalyse/dao/LiveChatDataMapper.java
@@ -25,7 +25,7 @@ public interface LiveChatDataMapper extends BaseMapper<LiveChatData> {
     void insertNotExists(LiveChatData liveChatData);
 
     @Select("select id,live_date,author_image,author_name,message,time_in_seconds,time_text,timestamp,emotes_count " +
-            "from live_chat_data where live_date = #{liveDate} and message like CONCAT('%',#{message},'%') ")
+            "from live_chat_data where live_date = #{liveDate} and message like '%' || #{message} || '%' ")
     List<LiveChatData> selectByKeyWords(@Param("liveDate") String liveDate, @Param("message") String message);
 
     @Select("select * from live_chat_data where live_date = #{liveDate} and time_in_seconds > 0 order by id limit 1")
@@ -37,7 +37,7 @@ public interface LiveChatDataMapper extends BaseMapper<LiveChatData> {
     @Insert("insert into live_chat_data(live_date, author_image, author_name, author_id, message, sc_info, sc_amount, TIMESTAMP, emotes_count) " +
             "select live_date, author_image, author_name, author_id, message, sc_info, sc_amount, TIMESTAMP, emotes_count " +
             "  from living_chat_data a " +
-            " where live_date like CONCAT(#{liveDate},'%') " +
+            " where live_date like #{liveDate} || '%' " +
             "   and not exists (select 1 from live_chat_data b where b.timestamp = a.TIMESTAMP) " +
             " order by TIMESTAMP desc")
     Integer asyncLivingChatData(@Param("liveDate") String liveDate);
